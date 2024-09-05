0f492bf994737f9df814b8082398464df027e7f0
Petr <56717039+pkonetskiy@users.noreply.github.com>
2024-09-05 17:17:24 +0200

    Added support SuiteCRM 7.14.x

--- SugarModules/custom/Extension/application/Ext/JSGroupings/newCustomFields.php
+++ SugarModules/custom/Extension/application/Ext/JSGroupings/newCustomFields.php
@@ -5,21 +5,17 @@ if (!defined('sugarEntry') || !sugarEntry) {die('Not A Valid Entry Point');}
  * BFtool new_Fields_types 1.0 2021-01-28
  *  
  ********************************************************************************/
-// unset($js_groupings[$sugar_field_grp]['include/SugarFields/Fields/Collection/SugarFieldCollection.js']);
-// $js_groupings = array(
-//        $sugar_field_grp = array(
-//        'custom/include/SugarFields/Fields/Collection/SugarFieldCollection.js' => 'include/javascript/sugar_field_grp.js',
-//        'custom/include/SugarFields/Fields/Collection_files/SugarFieldCollection_files.js' => 'include/javascript/sugar_field_grp.js',
-//        'include/SugarFields/Fields/Datetimecombo/Datetimecombo.js' => 'include/javascript/sugar_field_grp.js',
-//    ),
-// );
 
-// Remove old SugarFieldCollection add new elements to sugar_field_grp.js
-for($i=0; $i < count($js_groupings); $i++){
-    if (isset($js_groupings[$i]['include/SugarFields/Fields/Collection/SugarFieldCollection.js'])){
-        unset($js_groupings[$i]['include/SugarFields/Fields/Collection/SugarFieldCollection.js']);
-        $js_groupings[$i]['custom/include/SugarFields/Fields/Collection/SugarFieldCollection.js'] = 'include/javascript/sugar_field_grp.js';
-        $js_groupings[$i]['custom/include/SugarFields/Fields/Collection_files/SugarFieldCollection_files.js'] = 'include/javascript/sugar_field_grp.js';
-        break;
+$customScripts = [
+    'custom/include/SugarFields/Fields/Collection/SugarFieldCollection.js' => 'include/javascript/sugar_field_grp.js',
+    'custom/include/SugarFields/Fields/Collection_files/SugarFieldCollection_files.js' => 'include/javascript/sugar_field_grp.js',
+];
+
+foreach ($js_groupings as $keyGroup => $valueGroup) {
+    foreach ($valueGroup as $keyElem => $valueElem) {
+        if ($keyElem == 'include/SugarFields/Fields/Collection/SugarFieldCollection.js'){
+            unset($js_groupings[$keyGroup][$keyElem]);
+            $js_groupings[$keyGroup] = array_merge($js_groupings[$keyGroup], $customScripts);
+        }
     }
 }
